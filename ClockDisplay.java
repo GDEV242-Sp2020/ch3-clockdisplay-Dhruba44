2d3d4df84c4620cee77eb8188d02bfd5f896aade
Dhruba44 <60355023+Dhruba44@users.noreply.github.com>
2020-02-10 13:12:25 -0500

    Initial comment for testing
    testing

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -9,6 +9,7 @@
  * and reacts by incrementing the display. This is done in the usual clock
  * fashion: the hour increments when the minutes roll over to zero.
  * 
+ * This is meant for 12hoursInternal
  * @author Michael Kölling and David J. Barnes
  * @version 2011.07.31
  */
